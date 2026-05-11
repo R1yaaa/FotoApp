d9bd23d97af75afc6e0655f9e345fa753ce5564d
Riya <170878922+R1yaaa@users.noreply.github.com>
2026-05-11 12:57:50 +0200

    layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,34 @@
-<h1 class="text-6xl font-bold text-red-500">
-	Hallo Tailwind
-</h1>
+<div class="p-6">
+	<div class="mx-10 flex h-[85vh] flex-col overflow-hidden rounded-2xl bg-black">
+
+        <!-- KAMERA -->
+		<div class="flex flex-1 items-center justify-center text-white">
+			hier kommt kamera hin
+		</div>
+        <!-- BALKEN -->
+        <div class="flex h-18 items-center justify-center bg-yellow-200 text-black">
+			LOGO / TEXT
+		</div>
+	</div>
+
+    <!-- BUTTONS -->
+    <div class="mx-10 mt-4 flex gap-4 font-medium justify-center">
+
+        <button class="rounded-2xl bg-gray-300 p-4">
+            Kamera wechseln
+        </button>
+
+		<button class="rounded-2xl bg-green-400 p-4">
+			Foto aufnehmen
+		</button>
+
+		<button class="rounded-2xl bg-red-400 p-4">
+			Foto entfernen
+		</button>
+
+		<button class="rounded-2xl bg-blue-400 p-4">
+			Drucken
+		</button>
+	</div>
+</div>
+
